7ae1f5fa141e08698ddde920465639984e80305b
poorvigupta-a <poorvigupta1702@gmail.com>
2025-11-09 18:58:51 +0530

    Add files via upload

---
+++ differentdatatypes.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<limits.h>
+#include<float.h>
+int main()
+{
+    int intMin = INT_MIN;
+    
+    printf("Int Min Value :- %d\n",intMin);
+    printf("Int Max Value :- %d\n",INT_MAX);
+    printf("Float Max Value :- %f\n",FLT_MAX);
+    printf("Float Min Value :- %f\n",FLT_MIN);
+    
+    printf("\n");
+    return 0; 
+}
